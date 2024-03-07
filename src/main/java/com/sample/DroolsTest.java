84cd4d3fed258cfc61782825ac452d56f854b1f2
martodmih <martodmih@gmail.com>
2024-03-07 21:48:40 +0100

    testirani raznovrsni slucajevi

---
+++ src/main/java/com/sample/DroolsTest.java
@@ -0,0 +1,60 @@
+package com.sample;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+
+/**
+ * This is a sample class to launch a rule.
+ */
+public class DroolsTest {
+
+    public static final void main(String[] args) {
+        try {
+        	KieServices ks = KieServices.Factory.get();
+    	    KieContainer kContainer = ks.getKieClasspathContainer();
+        	KieSession kSession = kContainer.newKieSession("ksession-rules");
+
+            
+        	Room r = new Room();
+        	Team t = new Team ();
+        	Genre g = new Genre();
+        	
+        	
+        	r.setEvent(false);
+            r.setDuration(Duration.HOURS);
+            
+        	t.setGamesPlayed(4);
+        	t.setGamesSolved(4);
+        	t.setNumberOfPlayers(5);
+        	t.setYears(45);
+        	t.setTeamwork(true);
+        	t.setGoal(Goal.ACTION);
+        	t.setTeamwork(true);
+        	t.setActivityToHate(Activity.PAINTBALL);
+        	t.setActivityToLike(Activity.GAMESCLUB);
+        	
+        	g.setAmbiental(1);
+        	g.setMysterious(2);
+        	
+        	if (t.getNumberOfPlayers()>6 && !r.isEvent()) {
+        		throw new Exception("Maksimalan broj ljudi koji moze igrati jednu igru je 6. Ukoliko vas je vise i hocete da rezervisete dve, to se gleda kao dogadjaj.");
+        	}
+        	kSession.insert(g);
+            kSession.insert(t);
+            kSession.insert(r);
+            
+            kSession.fireAllRules();
+            System.out.println(r);
+            System.out.println(t);
+            System.out.println(g);
+            
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+    }
+
+    
+    
+
+}
